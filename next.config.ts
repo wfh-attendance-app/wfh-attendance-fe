df4d7ae39ed2f9c81b73599970c2a3e689526ce5
alvinxrww <alvin.xavier@ui.ac.id>
2025-04-13 10:25:08 +0700

    fix: change of backend APIs

--- next.config.ts
+++ next.config.ts
@@ -8,22 +8,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: '/api/auth/:path*',
-        destination: 'http://34.122.21.18:4000/api/auth/:path*'
-      },
-      {
-        source: '/api/attendance/:path*',
-        destination: 'http://35.193.28.139:4002/api/employee/attendance/:path*'
-      },
-      {
-        source: '/api/admin/:path*',
-        destination: 'http://35.232.21.216:4001/api/admin/:path*'
-      }
-    ];
-  }
 };
 
 export default nextConfig;
